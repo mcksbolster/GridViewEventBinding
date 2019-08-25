2fc2ea86c2c0b4cb177cdb8410386fea01bc414e
Sean <smbolster@gmail.com>
2019-08-25 07:54:22 -0400

    Fleshed Person model

--- GridViewEventBinding/Models/Person.cs
+++ GridViewEventBinding/Models/Person.cs
@@ -8,6 +8,13 @@ namespace GridViewEventBinding.Models
 {
     public class Person
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+
         
     }
 }
